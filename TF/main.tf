34dd0fb2523ca01f2cb4711a62aeab179f40d29a
olegign82 <olegign@gmail.com>
2024-08-29 22:00:34 -0500

    Update main.tf

--- TF/main.tf
+++ TF/main.tf
@@ -25,3 +25,24 @@ module "flux_bootstrap" {
 module "tls_private_key" {
   source         = "github.com/den-vasyliev/tf-hashicorp-tls-keys"
 }
+
+module "gke-workload-identity" {
+	source 				      = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
+	use_existing_k8s_sa = true
+	name			        	= "kustomize-controller"
+	namespace		      	= "flux-system"
+	project_id	    		= var.GOOGLE_PROJECT
+	cluster_name	    	= "main"
+	location			      = var.GOOGLE_REGION
+	annotate_k8s_sa	  	= true
+	roles 				      = ["roles/cloudkms.cryptoKeyEncrypterDecrypter"]
+}
+
+module "kms" {
+  source          = "github.com/den-vasyliev/terraform-google-kms"
+  project_id	   	= var.GOOGLE_PROJECT
+  keyring         = "sops-flux"
+  location		    = "global"
+  keys            = ["sops-key-flux"]
+  prevent_destroy = false
+}
